a426bffd27585daf4be4bd07951821d18112401c
Neto, Roberto Ehlert <Roberto.Neto@unisys.com>
2019-02-15 18:17:56 -0200

    Unisys.AppMigration - Start Project

--- AppMigration/Form1.Designer.cs
+++ AppMigration/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btConsulta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btConsulta
+            // 
+            this.btConsulta.Location = new System.Drawing.Point(394, 348);
+            this.btConsulta.Name = "btConsulta";
+            this.btConsulta.Size = new System.Drawing.Size(75, 23);
+            this.btConsulta.TabIndex = 0;
+            this.btConsulta.Text = "Consulta";
+            this.btConsulta.UseVisualStyleBackColor = true;
+            this.btConsulta.Click += new System.EventHandler(this.btConsulta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btConsulta);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btConsulta;
     }
 }
 
--- AppMigration/Form1.cs
+++ AppMigration/Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace AppMigration
 {
@@ -16,5 +18,59 @@ namespace AppMigration
         {
             InitializeComponent();
         }
-    }
+
+        private void btConsulta_Click(object sender, EventArgs e)
+        {
+            CTEGOLEntities2 Cte = new CTEGOLEntities2();
+            List<tbMigrationFile1> OpenAwbs = Cte.tbMigrationFile1.ToList();
+
+            ProcessOpenAWBs(OpenAwbs, Cte);
+        }
+
+        private void InsertMassData()
+        {
+           //string connString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+           // var data = System.IO.File.ReadAllLines(@"C:\TEMP\teste.csv");
+
+           // for (int i = 1; i < data.Count() - 1; i++)
+                
+        }
+
+        private void ProcessOpenAWBs(List<tbMigrationFile1> OpenAwbs, CTEGOLEntities2 conn)
+        {
+            List<tbMigrationFile8> AwbsCompletas = new List<tbMigrationFile8>();
+            int count = 0;
+            foreach (tbMigrationFile1 OpenAwb in OpenAwbs)
+            {
+
+                if ((!string.IsNullOrEmpty(OpenAwb.AWB_ID)) && (!string.IsNullOrEmpty(OpenAwb.ODN)))
+                {
+                    Int64 erpCode = Int64.Parse(OpenAwb.ODN.Substring(3));
+                    try
+                    {
+                        var awb = conn.tbctes
+                            .Where(w => w.erpCode.Equals(erpCode)).First();
+
+                        tbMigrationFile8 file8 = new tbMigrationFile8();
+                        
+                        file8.OND = OpenAwb.ODN;
+                        file8.ChaveCte = awb.eKey;
+                        file8.AWB_ID = OpenAwb.AWB_ID;
+                        count++;
+
+                        conn.tbMigrationFile8.Add(file8);
+                        //conn.SaveChanges();
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show("Erro: " + e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            conn.SaveChanges();        
+
+        MessageBox.Show("Operação Concluída.\n " + count + " Registros processados.", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        
+        }
+}
 }
